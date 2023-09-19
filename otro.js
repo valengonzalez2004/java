00917850c238e740541d8ba295e2c7192a5c2eed
valengonzalez2004 <valenracing12@gmail.com>
2023-09-18 23:32:11 -0300

    a la mitad

--- otro.js
+++ otro.js
@@ -1,153 +1,146 @@
+// let temas = 
+//     {
+//         tema1: "Geografia",
+//         tema2: "Escritores",
+//         tema3: "Pintores",
+//         tema4: "Montañas"
+//     }
 
-function bienvenidaUsuario() {
-    let saludo = prompt("Bienvenido, ingrese su nombre:");
-    alert("Bienvenido/a " + saludo + " al juego culturizate");
-    return saludo;
+//     let temasstr=JSON.stringify(temas);
+//     //let temaspar=JSON.parse(temasstr);
+//     localStorage.setItem ("ObjetoTemas", temasstr);
+//     let datosStorage = localStorage.getItem("ObjetoTemas");
+//     let temasconv = JSON.parse(datosStorage);
+
+//     alert (temasconv.tema1);
+
+
+
+
+
+// let preguntaActual = 0; // Debes declarar e inicializar la variable preguntaActual
+
+// const preguntas = [
+//     {
+//         pregunta: "¿Cuál es la capital de Francia?",
+//         respuestaCorrecta: "PARIS"
+//     },
+//     {
+//         pregunta: "¿Quién escribió Don Quijote de la Mancha?",
+//         respuestaCorrecta: "MIGUEL DE CERVANTES"
+//     },
+//     {
+//         pregunta: "¿Quien pinto la Mona Lisa?",
+//         respuestaCorrecta: "LEONARDO DA VINCI"
+//     },
+//     {
+//         pregunta: "¿Cuál es la montaña más alta del mundo?",
+//         respuestaCorrecta: "MONTE EVEREST"
+//     }
+// ];
+
+// const botonesRespuesta = document.getElementsByClassName("juego");
+
+// function mostrarPregunta() {
+//     if (preguntaActual < preguntas.length) {
+//         document.querySelector("h1").textContent = "CULTURIZATE ";
+//         document.querySelector("p").textContent = preguntas[preguntaActual].pregunta;
+//     } else {
+//         alert("¡Has completado todas las preguntas!");
+//     }
+// }
+
+// function avanzarPregunta() {
+//     preguntaActual++;
+//     mostrarPregunta();
+// }
+
+// function comprobarRespuesta(respuesta) {
+//     const respuestaCorrecta = preguntas[preguntaActual].respuestaCorrecta;
+//     if (respuesta === respuestaCorrecta) {
+//         alert("¡Respuesta correcta!");
+//         avanzarPregunta(); // Avanza a la siguiente pregunta
+//     } else {
+//         alert("Respuesta incorrecta. Inténtalo de nuevo.");
+//     }
+// }
+
+// for (let i = 0; i < botonesRespuesta.length; i++) {
+//     botonesRespuesta[i].addEventListener("click", function () {
+//         comprobarRespuesta(botonesRespuesta[i].textContent);
+//     });
+// }
+
+// mostrarPregunta();
+let temas = {
+    tema1: "Geografía",
+    tema2: "Escritores",
+    tema3: "Pintores",
+    tema4: "Montañas"
 }
 
-function eleccion() {
-    alert("Elija una opcion: ");
-}
+let temasstr = JSON.stringify(temas);
+localStorage.setItem("ObjetoTemas", temasstr);
+let datosStorage = localStorage.getItem("ObjetoTemas");
+let temasconv = JSON.parse(datosStorage);
 
-function eleccion2() {
-    let opcion;
+let preguntaActual = 0;
 
-    while (true) {
-        opcion = parseInt(prompt("1.Jugar \n2 Informacion de paises \n0.Salir"));
+const preguntas = [
+    {
+        pregunta: "¿Cuál es la capital de Francia?",
+        respuestaCorrecta: "PARIS"
+    },
+    {
+        pregunta: "¿Quién escribió Don Quijote de la Mancha?",
+        respuestaCorrecta: "MIGUEL DE CERVANTES"
+    },
+    {
+        pregunta: "¿Quién pintó la Mona Lisa?",
+        respuestaCorrecta: "LEONARDO DA VINCI"
+    },
+    {
+        pregunta: "¿Cuál es la montaña más alta del mundo?",
+        respuestaCorrecta: "MONTE EVEREST"
+    }
+];
 
-        if (opcion === 1) {
-            jugar();
-        } else if (opcion === 2) {
-            mostrarInformacionPaises();
-        } else if (opcion === 0) {
-            alert("Has salido del juego. ¡Hasta luego!");
-            break;
-        } else {
-            alert("Elija una opcion válida.");
-        }
+const botonesRespuesta = document.getElementsByClassName("juego");
+
+function mostrarPregunta() {
+    if (preguntaActual < preguntas.length) {
+        //document.querySelector("h1").textContent = "CULTURIZATE ";
+        document.querySelector("p").textContent = preguntas[preguntaActual].pregunta;
+    } else {
+        alert("¡Has completado todas las preguntas!");
     }
 }
 
-function jugar() {
-    alert("Bienvenido al juego normal, tienes 3 vidas, así que atento!");
+function avanzarPregunta() {
+    preguntaActual++;
+    mostrarPregunta();
 
-    let vidas = 3;
-
-    for (let nivel = 1; nivel <= 1; nivel++) {
-        // Lógica del juego aquí...
-        alert("GEOGRAFIA");
-
-        let nivel1 = parseInt(prompt("¿Cuál es la capital de Francia? \n1.MADRID \n2.ROMA \n3.PARIS \n4.LONDRES"));
-
-        if (nivel1 == 3) {
-            alert("Correcto. Avanzas al siguiente nivel.");
-        } else {
-            vidas--;
-            alert("Incorrecto. Te quedan " + vidas + " vidas.");
-        }
-        alert("ESCRITORES");
-        let nivel2 = parseInt(prompt("¿Quién escribió Don Quijote de la Mancha? \n1.Miguel de Cervantes \n2.Gabriel García Márquez \n3.Leo Tolstoy \n4.William Shakespeare"));
-
-
-        if (nivel2 == 1) {
-            alert("Correcto. Avanzas al siguiente nivel.");
-            // alert("NIVEL " + nivel);
-
-        } else {
-            vidas--;
-            alert("Incorrecto. Te quedan " + vidas + " vidas.");
-        }
-
-        // alert("NIVEL " + nivel);
-        alert("PINTORES");
-        let nivel3 = parseInt(prompt("¿Quien pinto la Mona Lisa? \n1.VICENTE VAN GOGH \n2.PABLO PICASSO \n3.LEONARDO DA VINCI \n4.MICHELANGELO BUONARROTI"))
-
-        if (nivel3 == 3) {
-            alert("Correcto. Avanzas al siguiente nivel.");
-            // alert("NIVEL " + nivel);
-        } else {
-            vidas--;
-            alert("Incorrecto. Te quedan " + vidas + " vidas.");
-        }
-
-        // alert("NIVEL " + nivel);
-        alert("GEOGRAFIA");
-
-        let nivelUltimo = parseInt(prompt("¿Cuál es la montaña más alta del mundo? \n1.KILIMANJARO \n2.MONT BLANC \n3.MONTAÑA DE LA MESA \n4.MONTE EVEREST"))
-        if (nivelUltimo == 4) {
-            alert("Correcto!");
-
-        }
-        if (vidas === 0) {
-            alert("Has perdido el juego");
-            break; // Sale del juego si las vidas llegan a 0
-        }
-    }
-
-    const opcionContinuar = parseInt(prompt("¿Deseas jugar nuevamente? (1: Sí, 0: No)"));
-    if (opcionContinuar === 0) {
-        alert("Gracias por jugar. ¡Hasta luego!");
+    // Mostrar el tema correspondiente después de avanzar la pregunta
+    if (preguntaActual < Object.keys(temasconv).length) {
+        const temaActual = Object.keys(temasconv)[preguntaActual];
+        alert("Tema actual: " + temasconv[temaActual]);
     }
 }
 
-function mostrarInformacionPaises() {
-    alert("Bienvenido al apartado de informaciones de países");
-    alert("Presta atención y elige un país");
-
-    const paises = [
-        {
-            pais: "1. Argentina",
-            capital: "Buenos Aires",
-            habitantes: 46.044703,
-            comida: "Asado",
-        },
-        {
-            pais: "2. Rusia",
-            capital: "Moscu",
-            habitantes: 145.558000,
-            comida: "Borsch",
-        },
-        {
-            pais: "3. Estados Unidos",
-            capital: "Washington",
-            habitantes: 332.314000,
-            comida: "Hamburguesas y hot dogs",
-        },
-    ];
-
-    let opciones = "";
-
-    for (pais of paises) {
-        opciones += `${pais.pais}\n`;
-    }
-
-    let eleccionIndex;
-
-    while (true) {
-        eleccionIndex = parseInt(prompt(opciones)) - 1;
-
-        if (eleccionIndex < 0 || eleccionIndex >= paises.length) {
-            alert("País no encontrado. Por favor, elija nuevamente o presione 0 para salir.");
-        } else {
-            const paisElegido = paises[eleccionIndex];
-            alert("Elegiste el país " + paisElegido.pais);
-            alert("Capital: " + paisElegido.capital);
-            alert("Habitantes: " + paisElegido.habitantes);
-            alert("Comida típica: " + paisElegido.comida);
-            alert("Gracias por probarnos, estamos en fase beta!");
-            alert("Adiós :)");
-            break;
-        }
+function comprobarRespuesta(respuesta) {
+    const respuestaCorrecta = preguntas[preguntaActual].respuestaCorrecta;
+    if (respuesta === respuestaCorrecta) {
+        alert("¡Respuesta correcta!");
+        avanzarPregunta();
+    } else {
+        alert("Respuesta incorrecta. Inténtalo de nuevo.");
     }
 }
 
-bienvenidaUsuario();
-eleccion();
-eleccion2();
+for (let i = 0; i < botonesRespuesta.length; i++) {
+    botonesRespuesta[i].addEventListener("click", function () {
+        comprobarRespuesta(botonesRespuesta[i].textContent);
+    });
+}
 
-
-
-
-
-
-
+mostrarPregunta();
