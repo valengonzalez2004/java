e4065454c0d45339f9749573f513226bef2cbaa1
valengonzalez2004 <valenracing12@gmail.com>
2023-09-19 23:38:33 -0300

    Error de preguntas

--- javascript/otro.js
+++ javascript/otro.js
@@ -19,17 +19,26 @@ let temas = [{
 localStorage.setItem("ObjetoTemas", JSON.stringify(temas));
 let temasconv = JSON.parse(localStorage.getItem("ObjetoTemas"));
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("jugar").addEventListener("click", function () {
+    console.log("DOMContentLoaded event fired.");
+    document.getElementById("lista").addEventListener("click", function () {
         let temasAlmacenados = "Algunos de los temas que veremos hoy:\n";
-        for (let i = 0; i < temas.length; i++) {
-            temasAlmacenados +=  temas[i].tema1 + "\n";
+        for (let i = 0; i < temasconv.length; i++) {
+            temasAlmacenados += temasconv[i].tema1 + "\n";
         }
         alert(temasAlmacenados);
-    
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("jugar").addEventListener("click", function () {
+        let temasAlmacenados = "Algunos de los temas que veremos hoy:\n";
+        for (let i = 0; i < temas.length; i++) {
+            temasAlmacenados += temas[i].tema1 + "\n";
+        }
+        alert(temasAlmacenados);
 
+    });
+});
 
 let preguntaActual = 0;
 
@@ -56,44 +65,91 @@ const botonesRespuesta = document.getElementsByClassName("juego");
 
 function mostrarPregunta() {
     if (preguntaActual < preguntas.length) {
+        const pregunta = preguntas[preguntaActual];
+        document.getElementById("pregunta").textContent = pregunta.pregunta;
+    } else {
+        alert("¡Has respondido todas las preguntas!");
     }
 }
 
-function avanzarPregunta() {
-    preguntaActual++;
-    mostrarPregunta();
-}
 
+const avanzarPregunta = () => preguntaActual++ && mostrarPregunta();
 
 
 function comprobarRespuesta(respuesta) {
     const respuestaCorrecta = preguntas[preguntaActual].respuestaCorrecta;
-    if (respuesta.toUpperCase() === respuestaCorrecta) {
-        alert("¡Respuesta correcta!");
-        avanzarPregunta(); // Avanza solo si la respuesta es correcta
-    } else {
-        alert("Respuesta incorrecta. Inténtalo de nuevo.");
-    }
+    const esRespuestaCorrecta = respuesta === respuestaCorrecta;
+    const mensaje = esRespuestaCorrecta
+        ? "¡Respuesta correcta!"
+        : "Respuesta incorrecta. Inténtalo de nuevo.";
+    alert(mensaje);
+    avanzarPregunta();
 }
 
 
-
-
-// function comprobarRespuesta(respuesta) {
-//     const respuestaCorrecta = preguntas[preguntaActual].respuestaCorrecta;
-//     if (respuesta.toUpperCase() === respuestaCorrecta) { // Convertir a mayúsculas para comparar sin importar mayúsculas/ minúsculas
-//         alert("¡Respuesta correcta!");
-//         avanzarPregunta();
-//     } else {
-//         alert("Respuesta incorrecta. Inténtalo de nuevo.");
-//     }
-// }
-
 for (let i = 0; i < botonesRespuesta.length; i++) {
-    botonesRespuesta[i].addEventListener("click",function () {
+    botonesRespuesta[i].addEventListener("click", function () {
         comprobarRespuesta(botonesRespuesta[i].textContent);
     });
 }
 
 mostrarPregunta();
-avanzarPregunta();
+
+
+
+// let preguntaActual = 0;
+
+// const preguntas = [
+//     {
+//         pregunta: "¿Cuál es la capital de Francia?",
+//         respuestaCorrecta: "PARIS"
+//     },
+//     {
+//         pregunta: "¿Quién escribió Don Quijote de la Mancha?",
+//         respuestaCorrecta: "MIGUEL DE CERVANTES"
+//     },
+//     {
+//         pregunta: "¿Quién pintó la Mona Lisa?",
+//         respuestaCorrecta: "LEONARDO DA VINCI"
+//     },
+//     {
+//         pregunta: "¿Cuál es la montaña más alta del mundo?",
+//         respuestaCorrecta: "MONTE EVEREST"
+//     }
+// ];
+
+// const botonesRespuesta = document.getElementsByClassName("juego");
+
+// function mostrarPregunta() {
+//     if (preguntaActual < preguntas.length) {
+//         const pregunta = preguntas[preguntaActual];
+//         document.getElementById("pregunta").textContent = pregunta.pregunta;
+//     }
+// }
+
+// function avanzarPregunta() {
+//     preguntaActual++;
+//     mostrarPregunta();
+// }
+
+
+
+// function comprobarRespuesta(respuesta) {
+//     const respuestaCorrecta = preguntas[preguntaActual].respuestaCorrecta;
+//     if (respuesta.toUpperCase() === respuestaCorrecta) {
+//         alert("¡Respuesta correcta!");
+//         avanzarPregunta(); // Avanza solo si la respuesta es correcta
+//     } else {
+//         alert("Respuesta incorrecta. Inténtalo de nuevo.");
+//     }
+// }
+
+
+
+// for (let i = 0; i < botonesRespuesta.length; i++) {
+//     botonesRespuesta[i].addEventListener("click", function () {
+//         comprobarRespuesta(this.innerText);
+//     });
+// }
+
+// mostrarPregunta();
